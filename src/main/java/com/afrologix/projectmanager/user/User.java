ae3e200d7efec0abbee3ba7635e15086a85fa825
Afesoh Brain <brainafesoh@gmail.com>
2021-01-25 09:30:02 +0100

    wtf

--- src/main/java/com/afrologix/projectmanager/user/User.java
+++ src/main/java/com/afrologix/projectmanager/user/User.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(schema = "public")
 /**
  * User
  */
